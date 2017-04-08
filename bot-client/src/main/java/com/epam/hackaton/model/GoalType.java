9696611598cfd1d400a5d5562d40302651010c91
Catherine <Novikova>
2017-04-08 12:48:44 +0300

    Added creating request body for jobs and job goals.

---
+++ bot-client/src/main/java/com/epam/hackaton/model/GoalType.java
@@ -0,0 +1,23 @@
+package com.epam.hackaton.model;
+
+/**
+ * Created by Kanstantsin_Tolstsik on 4/8/2017.
+ */
+public enum GoalType {
+
+    SIZE_WAITING("Waiting for size");
+
+    private String goalDescription;
+
+    GoalType(String goalDescription) {
+        this.goalDescription = goalDescription;
+    }
+
+    public String getGoalDescription() {
+        return goalDescription;
+    }
+
+    public void setGoalDescription(String goalDescription) {
+        this.goalDescription = goalDescription;
+    }
+}
---
+++ bot-client/src/main/java/com/epam/hackaton/model/JobType.java
@@ -0,0 +1,23 @@
+package com.epam.hackaton.model;
+
+/**
+ * Created by Kanstantsin_Tolstsik on 4/4/2017.
+ */
+public enum JobType {
+
+    GETTING_ITEM_DETAILS("Getting item details using URL");
+
+    private String jobDescription;
+
+    JobType(final String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+}
